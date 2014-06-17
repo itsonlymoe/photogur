788eb1bb3a247ae2beb9e632bf6c7cfe8112fb06
Moe <kenol.maurice@gmail.com>
2014-06-17 12:52:25 -0400

    Making each picture show at it's own URL using static routes

--- photogur/config/routes.rb
+++ photogur/config/routes.rb
@@ -57,4 +57,8 @@ end
 
 Photogur::Application.routes.draw do
   get 'pictures' => 'pictures#index'
+
+  get 'pictures/0' => 'pictures#picture0'
+  get 'pictures/1' => 'pictures#picture1'
+  get 'pictures/2' => 'pictures#picture2'
 end
